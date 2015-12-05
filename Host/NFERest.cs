5338810356361b9302ec44e0ff15631efeaed260
Marcus Lopes <marcusporteira@gmail.com>
2015-12-05 12:36:23 -0800

    push push

--- Host/NFERest.cs
+++ Host/NFERest.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelo.Contratos;
+using Modelo.Modelos.NFE;
 
 namespace Host
 {
-    class NFERest
+    public class NFERest : INFE
     {
+        public void EnviarNFE(RequestNFE request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseNFE NfeAssinada(int numeroNota)
+        {
+            return new ResponseNFE()
+            {
+                Assinatura = Guid.NewGuid().ToString(),
+                CNPJ = "111.111.111/0001-24",
+                ISS = 6.5m,
+                Valor = 1000,
+                RazaoSocial = "PTs Association"
+//            };
+        }
     }
 }
