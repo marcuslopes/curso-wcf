b5cb1a37c5572cc12305a259d8ef604fcd465567
Marcus Lopes <marcusporteira@gmail.com>
2015-12-05 12:28:33 -0800

    First commit

---
+++ Host/NFE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Contratos;
+using Modelo.Modelos.NFE;
+
+namespace Host
+{
+    public class NFE : INFE
+    {
+        public void EnviarNFE(RequestNFE request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseNFE NfeAssinada(int numeroNota)
+        {
+            return new ResponseNFE()
+            {
+                Assinatura = Guid.NewGuid().ToString(),
+                CNPJ = "111.111.111/0001-24",
+                ISS = 6.5m,
+                Valor = 1000,
+                RazaoSocial = "PTs Association"
+            };
+        }
+    }
+}
---
+++ Host/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Contratos;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CriarHost();
+            Console.WriteLine();
+        }
+
+        private static void CriarHost()
+        {
+            // A + B + C
+            string address = string.Format("http://{0}:9000/Servico/NFE", Environment.MachineName);
+
+            Console.WriteLine(address);
+
+            //Binding = Letra B
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            ServiceHost host = new ServiceHost(typeof(NFE), new Uri(address));
+
+            host.AddServiceEndpoint(
+                typeof (INFE), binding, new Uri(address));
+
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior()
+            {
+                HttpGetEnabled = true
+            };
+
+            host.Description.Behaviors.Add(smb);
+
+        }
+    }
+}
